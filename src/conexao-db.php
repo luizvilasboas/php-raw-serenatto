283cb91e9f0625e03a4fd495bc610007cf881302
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-24 16:16:48 -0300

    Usando configurações do arquivo .env em vez de ser hard-coded

--- src/conexao-db.php
+++ src/conexao-db.php
@@ -1,7 +1,15 @@
 <?php
 
-$usuario = "root";
-$senha = "root";
+$config = parse_ini_file(dirname(__FILE__) . "/../.env");
 
-$pdo = new PDO("mysql:host=localhost;dbname=serenatto", $usuario, $senha);
+$host = $config["DB_HOST"];
+$port = $config["DB_PORT"];
+$banco_de_dados = $config["DB_DATABASE"];
+$usuario = $config["DB_USERNAME"];
+$senha = $config["DB_PASSWORD"];
 
+try {
+    $pdo = new PDO("mysql:host={$host};port={$port};dbname={$banco_de_dados}", $usuario, $senha);
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
